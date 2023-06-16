810c190c5443f441f570a7f0822ffc00c70787d2
EbrahimAli-1 <ebrahimalirabea15@gmail.com>
2023-06-16 12:26:37 -0700

    Here

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+*/
+
+int main(void)
+{
+	char l, u;
+
+	for (l="a", u="A"; l <= "z", u <= "Z"; l++, u++)
+	{
+		putchar(l);
+		putchar(u);
+	}
+	putchar('\n');
+	return (0);
+}
