7e264aabd76c1580c608ee824696c4c28d023139
EbrahimAli-1 <ebrahimalirabea15@gmail.com>
2023-06-16 15:52:09 -0700

    Here

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - A program to print all single digit numbers and seperates each number with comma and white-space using putchar.
+ *
+ * Return: Always 0 (Success)
+*/
+
+
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar('0' + a);
+		putchar(", ");
+	}
+	putchar('\n');
+
+	return (0);
+}
