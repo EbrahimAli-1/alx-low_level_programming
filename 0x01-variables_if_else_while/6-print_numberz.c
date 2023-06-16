c7cca5b0812b2c640d204ca6fffc08f58dd007b6
EbrahimAli-1 <ebrahimalirabea15@gmail.com>
2023-06-16 15:12:13 -0700

    Here

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio,h>
+#include <stdlib.h>
+
+
+/**
+ * main - A program to print single numbers from 0 to 10 using putchar.
+ *
+ * Return: Always 0 (Success)
+*/
+
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
