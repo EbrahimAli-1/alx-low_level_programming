febe0a59faf699943491b98a014b8b4e223ad5c1
EbrahimAli-1 <ebrahimalirabea15@gmail.com>
2023-06-15 19:24:50 -0700

    Here

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-int main()
+
+/*A program that prints a line by using puts()*/
+
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return 0;
+return (0);
 }
