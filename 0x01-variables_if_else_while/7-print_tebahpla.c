6b547cf7a37962a7a9b6d1cf1568634e1db44509
EbrahimAli-1 <ebrahimalirabea15@gmail.com>
2023-06-16 15:31:14 -0700

    Here

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <strlib.h>
+
+/**
+ * main - A program to print lowercase alphabets reversed using putchat.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a >= 'a'; a--)
+	{
+		putchar(a);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
