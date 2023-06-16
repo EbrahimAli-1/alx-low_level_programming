51f50fa008c91d605ab24293cecfeaf8781b20ab
EbrahimAli-1 <ebrahimalirabea15@gmail.com>
2023-06-16 12:27:10 -0700

    Here

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
+/*
  *
  *
  *
@@ -14,7 +14,7 @@ int main(void)
 {
 	char l, u;
 
-	for (l="a", u="A"; l <= "z", u <= "Z"; l++, u++)
+	for (l = "a", u = "A"; l <= "z", u <= "Z"; l++, u++)
 	{
 		putchar(l);
 		putchar(u);
