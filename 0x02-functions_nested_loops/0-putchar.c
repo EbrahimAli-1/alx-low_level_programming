f85ef87fb4ef9aff1cbf04367075a38aec8255ab
EbrahimAli-1 <ebrahimalirabea15@gmail.com>
2023-06-21 18:42:13 -0700

    Here

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <strio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - A program to print _putchar.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	putchar("_putchar");
+	putchar('\n');
+	return (0);
+}
